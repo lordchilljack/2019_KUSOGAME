3ce45138732cb22ce8566cba82a68d45fb0af1c8
lordchilljack <lordchilljack@gmail.com>
2019-04-10 22:54:23 +0800

    20190410

---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour
+{
+    public float TimeCurrent = 0.0f;
+    public float TimeLimit = 30.0f;
+    public int ClothesTotal = 21;
+    public int Clothsleft = 21;
+
+    public int ClothRange() {
+
+    }
+
+    //
+    // 顯示主題、提示
+    //
+    void Question() {
+        switch () {
+
+
+        }
+    }
+
+    //
+    // 穿衣服
+    //
+    public void Wear( GameObject Cloth) {
+        if (Cloth) {
+            
+        }
+    }
+
+    // Wake is called when the scence is loaded
+    void Wake() {
+
+    }
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
