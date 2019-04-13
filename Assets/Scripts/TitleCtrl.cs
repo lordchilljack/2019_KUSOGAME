4b0ed429e0229e543a41c788fcba8fde69358c0f
Gunpow <arturiaxnight@gmail.com>
2019-04-13 22:06:37 +0800

    完成遊戲選單

---
+++ Assets/Scripts/TitleCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleCtrl : MonoBehaviour {
+
+
+	public void LeaveGame(){
+		Application.Quit ();
+	}
+
+
+	public void StartGame(){
+		SceneManager.LoadScene(1);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void OpenFacebook(){
+		Application.OpenURL("https://www.facebook.com/TryHarderOkay");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
